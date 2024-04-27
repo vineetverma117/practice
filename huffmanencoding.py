acd16155baf8b0968eed7a6a6bfdb24e0a1ec7e1
vineet verma <71189082+vineetverma117@users.noreply.github.com>
2024-04-27 12:23:28 +0530

    Create huffmanencoding.py

---
+++ huffmanencoding.py
@@ -0,0 +1,56 @@
+import heapq
+
+class HuffmanNode:
+    
+    def __init__(self, symbol, frequency, left=None, right=None):
+        self.symbol = symbol
+        self.frequency = frequency
+        self.left = left
+        self.right = right
+        #tree direction
+        self.huff = ''
+        
+    def __lt__(self, next):
+        return self.frequency < next.frequency
+ 
+class HuffmanTree:
+     
+    def convert_into_huffman_nodes(self, heap, char_array, freq_array):
+        for index in range(len(char_array)):
+            heapq.heappush(heap, HuffmanNode(char_array[index], freq_array[index]))
+            
+    def build_huffman_tree(self, heap):
+        while(len(heap) > 1):
+            left = heapq.heappop(heap)
+            right = heapq.heappop(heap)
+            
+            #direction for encode
+            left.huff = '0'
+            right.huff = '1'
+            
+            newnode = HuffmanNode(left.symbol+right.symbol, left.frequency+right.frequency, left=left, right=right)
+            heapq.heappush(heap, newnode)
+   
+    def printEncoding(self, node, val=''):
+        newVal = val + node.huff
+        
+        if(node.left):
+            self.printEncoding(node.left, newVal)
+        if(node.right):
+            self.printEncoding(node.right, newVal)
+            
+        if node.left == None and node.right == None:
+            print(f"{node.symbol}: {newVal}")
+        
+
+if __name__ == '__main__':
+    chars = ['a', 'b', 'c', 'd', 'e', 'f'] 
+    freq = [5, 9, 12, 13, 16, 45]
+    
+    nodes = []
+    obj = HuffmanTree()
+    obj.convert_into_huffman_nodes(nodes, chars, freq)
+    print(len(nodes))
+    obj.build_huffman_tree(nodes)
+    print(len(nodes))
+    obj.printEncoding(nodes[0], '')
